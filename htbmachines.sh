88daa6a5b0cdb8a163b256cfbd4b28cc7a8b9628
Sn0wBaall <157326896+Sn0wBaall@users.noreply.github.com>
2024-06-12 18:19:56 -0400

    Colors Update

--- htbmachines.sh
+++ htbmachines.sh
@@ -2,14 +2,14 @@
 
 ## COLOURS
 
-greenColour="\e[0;32m\033[1m"
+greenColour="\e[0;32m"
 endColour="\033[0m\e[0m"
-redColour="\e[0;31m\033[1m"
-blueColour="\e[0;34m\033[1m"
-yellowColour="\e[0;33m\033[1m"
-purpleColour="\e[0;35m\033[1m"
-turquoiseColour="\e[0;36m\033[1m"
-grayColour="\e[0;37m\033[1m"
+redColour="\e[0;31m"
+blueColour="\e[0;34m"
+yellowColour="\e[0;33m"
+purpleColour="\e[0;35m"
+turquoiseColour="\e[0;36m"
+grayColour="\e[0;37m"
 
 ## VARIABLES GLOBALES
 
